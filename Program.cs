788dc59658699df2b43789e8f1a9960cc99717df
Santiago <santigs211@gmail.com>
2025-07-08 23:58:24 -0300

    drawing window

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿
 using game_spaceship;
+using System.Drawing;
+Ventana ventana = new(170, 45, ConsoleColor.Black,
+ limiteSuperior: new Point(5, 5), limiteInferior: new Point(165, 40));
 
-Ventana ventana = new(800, 600);
+ventana.DibujarMarco();
+
 Console.ReadKey();
--- Ventana.cs
+++ Ventana.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Drawing;
 namespace game_spaceship;
 
 public class Ventana
@@ -7,10 +7,19 @@ public class Ventana
     public int Ancho { get; set; }
     public int Alto { get; set; }
 
-    public Ventana(int ancho, int alto)
+    public Point LimiteSuperior { get; set; }
+    public Point LimiteInferior { get; set; }
+
+    public ConsoleColor Color { get; set; }
+
+    public Ventana(int ancho, int alto, ConsoleColor color, Point limiteSuperior, Point limiteInferior)
     {
         Ancho = ancho;
         Alto = alto;
+        Color = color;
+        LimiteSuperior = limiteSuperior;
+        LimiteInferior = limiteInferior;
+
         init();
     }
 
@@ -19,6 +28,47 @@ public class Ventana
     {
         Console.SetWindowSize(Ancho, Alto);
         Console.Title = "Spaceship Game";
-        
+        Console.BackgroundColor = Color;
+        Console.Clear();
+
     }
+
+
+
+    public void DibujarMarco()
+    {
+        for (int i = LimiteSuperior.X; i < Ancho; i++)
+        {
+            Console.SetCursorPosition(i, LimiteSuperior.Y);
+            Console.Write("═");
+            Console.SetCursorPosition(i, LimiteInferior.Y);
+            Console.Write("═");
+        }
+
+        for (int i = LimiteSuperior.Y; i < LimiteInferior.Y; i++)
+        {
+            Console.SetCursorPosition(LimiteSuperior.X, i);
+            Console.Write("║");
+            Console.SetCursorPosition(LimiteInferior.X, i);
+            Console.Write("║");
+        }
+
+
+        Console.SetCursorPosition(LimiteSuperior.X, LimiteSuperior.Y);
+        Console.Write("╔");
+        Console.SetCursorPosition(LimiteInferior.X, LimiteSuperior.Y);
+        Console.Write("╗");
+        Console.SetCursorPosition(LimiteSuperior.X, LimiteInferior.Y);
+        Console.Write("╚");
+        Console.SetCursorPosition(LimiteInferior.X, LimiteInferior.Y);
+        Console.Write("╝");
+
+
+    
+    }
+
+
+
+
+
 }
